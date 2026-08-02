1355a77f51be4a5fd588eb46a70620441b275b2e
mukeshraj <mukeshraj338@gmail.com>
2026-08-03 03:11:42 +0530

    hello

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "TRUMP : Hey Iran, we are going to blast you!"
+echo "IRAN : Yes, please you are welcome"
+echo "TRUMP : Hey Iran I'm not joking"
+echo "IRAN : Hey Trump, we are also not joking"
