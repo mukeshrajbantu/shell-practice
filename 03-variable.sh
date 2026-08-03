171bcf2a6c260bf7221afeb62c881151de4ccf76
mukeshraj <mukeshraj338@gmail.com>
2026-08-03 18:10:20 +0530

    hello

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 : Hey $PERSON2, we are going to blast you!"
+echo "$PERSON2 : Yes, please you are welcome"
+echo "$PERSON1 : Hey $PERSON2 I'm not joking"
+echo "$PERSON2 : Hey $PERSON1, we are also not joking"
