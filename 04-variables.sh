087d4986fbf62597968d5efd944b5121d9d6b12d
mukeshraj <mukeshraj338@gmail.com>
2026-08-03 18:30:15 +0530

    hello

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,3 +4,9 @@ echo "Please enter your username::"
 read USER_NAME #here USER_NAME is variable, what ever you enter in the terminal stores in this variable.
 
 echo "Username is $USER_NAME"
+#to the password as well.
+echo "Please enter your password::"
+
+read PASSWORD
+
+echo "Password is $PASSWORD"
