b5abe4ae88437cde6bae22b31d9bb7743211487c
mukeshraj <mukeshraj338@gmail.com>
2026-08-03 16:38:31 +0530

    hello

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1= TRUMP
-PERSON2= IRAN
+PERSON1 = TRUMP
+PERSON2 = IRAN
 echo "$PERSON1 : Hey $PERSON2, we are going to blast you!"
 echo "$PERSON2 : Yes, please you are welcome"
 echo "$PERSON1 : Hey $PERSON2 I'm not joking"
