691f900153440cef569cc92ac6d260c3d9cf8efb
mukeshraj <mukeshraj338@gmail.com>
2026-08-06 19:48:00 +0530

    hello

---
+++ 07-data-types.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NUM1=10
+NUM2=30
+
+SUM=$(($NUM1+$NUM2))
+#one bracket for arthimetical operations and other once is to execute.
+echo "Sum is: $SUM"
+#Just variable name has been given.
