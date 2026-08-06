23b1391b28418418208d04309c34968863b98c55
mukeshraj <mukeshraj338@gmail.com>
2026-08-06 20:14:47 +0530

    hello

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 NUM1=30
-NUM2=30
+NUM2=30 #If LINUX given the number value would be 0.
 
 SUM=$(($NUM1+$NUM2))
 #one bracket for arthimetical operations and other once is to execute.
 echo "Sum is: $SUM"
 #Just variable name has been given.
+#ARRAY
+MOVIES=("RRR" "VARANASI" "PUSHPA") #index always starts from 0.
+#movies is variable and the movie names are values.
+#so the value's 1st position always starts from 0 = RRR and so on...
+#The positionning would be done by zero but the number would start from 1,2,3..
+echo "Movies are: ${MOVIES[@]}" #@ is for all
+#The square braceses we can give positionig or indexing.
+#to access all movies.
