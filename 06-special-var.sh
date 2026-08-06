0466f90dd8f58b0402a7e9c9fbba82aa433c6f0a
mukeshraj <mukeshraj338@gmail.com>
2026-08-06 15:37:12 +0530

    hello

--- 06-special-var.sh
+++ 06-special-var.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$@ #all variables passed to the script, in shell variables start with "$".
+$@ #all variables passed to the script, in shell variables start with $
 ######## Special Vars #########
 echo "All variables passed to script: $@"
 echo "Number of variables passed: $#"
