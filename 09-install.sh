8569e1c96b6dec67bce475cb897002d1509cf7bf
mukeshraj <mukeshraj338@gmail.com>
2026-08-07 18:53:22 +0530

    hello

--- 09-install.sh
+++ 09-install.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u) #condition 
 #check root acceses or not.
 if [ $USERID -ne 0 ]; then
-    echo " lease run this script with root access"
+    echo "Please run this script with root access"
     exit 1
 fi
     #echo "I'm continuing" 
