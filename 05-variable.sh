c08bf82d9d1b2784e41e291c495365dab694a020
mukeshraj <mukeshraj338@gmail.com>
2026-08-16 05:04:20 +0530

    hello

--- 05-variable.sh
+++ 05-variable.sh
@@ -5,11 +5,7 @@ TIMESTAMP=$(date)  #here date is command and TIMESTAMP is variable.
 echo "Time is: $TIMESTAMP"
 
 START_TIME=$(date +%s)
-
-
 sleep 10
-
-
 END_TIME=$(date +%s)
 
 TOTAL_TIME=$(($END_TIME-$START_TIME))
