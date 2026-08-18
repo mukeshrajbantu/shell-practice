498e35b375b68c0cb1028a9230483928ec6bb880
mukeshraj <mukeshraj338@gmail.com>
2026-08-19 01:59:36 +0530

    hello

--- 06-special-var.sh
+++ 06-special-var.sh
@@ -4,7 +4,7 @@
 echo "All variables passed to script: $@"
 echo "Number of variables passed: $#" 
 echo "First variable: $1"
-# echo "Second variable: $2"
+echo "Second variable: $2"
 # echo "Script name: $0"
 # echo "Who is running this: $USER"
 # echo "Which directory: $PWD"
