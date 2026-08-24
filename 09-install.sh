87543640f88a6779dd8f3118aa5a95d92f9e6dfc
mukeshraj <mukeshraj338@gmail.com>
2026-08-24 19:05:44 +0530

    hello

--- 09-install.sh
+++ 09-install.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 #sudo id -u = 0
-# USERID=$(id -u) #condition 
-# #check root acceses or not.
-# if [ $USERID -ne 0 ]; then
-#     echo "Please run this script with root access"
-#     exit 1
-# fi
-#     #echo "I'm continuing" 
-#     echo "Install MySQL"
-#     dnf install mysql -y 
-# if [ $? -ne 0 ]; then 
-#     echo "Insatalling MySQL is..... FAILED"
-#     exit 1
-# else 
-#     echo "Installing MySQL is........ SUCCESS"
-# fi
 USERID=$(id -u) #condition
 if [ $USERID -ne 0 ]; then
     echo "Please run the script with root access"
     exit 1
 fi
     echo "I'm continuing"
+    dnf list installed mysql
+if [ $? -eq 0 ]; then
+    echo "MySQL is already isntalled.... SKIPPING"
+else
     echo "Intall MySQL"
     dnf install mysql -y
-if [ $? -ne 0 ]; then 
+    if [ $? -ne 0 ]; then 
     echo "Installing mysql is ... FAILED"
     exit 1
 else 
     echo "Installing MySQL is ..... SUCCESSs"
+fi
+fi
+
+
+    dnf list installed nginx
+if [ $? -eq 0 ]; then
+    echo "Nginx is already installed...SKIPPING"
+else 
+    echo "Insatlling nginx"
+    dnf install nginx -y
+fi
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx is ..... FAILED"
+    exit 1
+else
+    echo "Installing Nginx is... SUCCESS"
 fi
