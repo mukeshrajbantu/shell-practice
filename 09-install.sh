65e87f4b400aa57812051207e3a4d1f8df186494
mukeshraj <mukeshraj338@gmail.com>
2026-08-24 17:00:34 +0530

    hello

--- 09-install.sh
+++ 09-install.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 #sudo id -u = 0
-USERID=$(id -u) #condition 
-#check root acceses or not.
+# USERID=$(id -u) #condition 
+# #check root acceses or not.
+# if [ $USERID -ne 0 ]; then
+#     echo "Please run this script with root access"
+#     exit 1
+# fi
+#     #echo "I'm continuing" 
+#     echo "Install MySQL"
+#     dnf install mysql -y 
+# if [ $? -ne 0 ]; then 
+#     echo "Insatalling MySQL is..... FAILED"
+#     exit 1
+# else 
+#     echo "Installing MySQL is........ SUCCESS"
+# fi
+USERID=$(id -u) #condition
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root access"
+    echo("Please run the script with root access")
     exit 1
 fi
-    #echo "I'm continuing" 
-    echo "Install MySQL"
-    dnf install mysql -y 
+    echo "I'm continuing"
+    echo "Intall MySQL"
+    dnf install mysql -y
 if [ $? -ne 0 ]; then 
-    echo "Insatalling MySQL is..... FAILED"
+    echo "Installing mysql is ... FAILED"
     exit 1
 else 
-    echo "Installing MySQL is........ SUCCESS"
+    echo "Installing MySQL is ..... SUCCESSs"
 fi
-
