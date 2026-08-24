4d64d98cf27bdb2278eb6c0f295ba4444adb88f1
mukeshraj <mukeshraj338@gmail.com>
2026-08-25 02:18:32 +0530

    hello

--- 10-logs.sh
+++ 10-logs.sh
@@ -27,7 +27,7 @@ else
 VALIDATE MySQL $?
 fi
 
-    echo "I'm continuing"
+    #echo "I'm continuing"
     dnf list installed nginx &>> $LOGS_FILE
 if [ $? -eq 0 ]; then
     echo "Nginx is already installed...SKIPPING"
