9772d3b2f4d9e49334d863b18bf182b657bb9e7b
mukeshraj <mukeshraj338@gmail.com>
2026-08-24 16:18:41 +0530

    hello

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -3,7 +3,7 @@
 NUM1=30
 NUM2=30 #If LINUX given the number value would be 0.
 
-SUM=$(($NUM1+$NUM2))
+SUM=$(($NUM1*$NUM2))
 #one bracket for arthimetical operations and other once is to execute.
 echo "Sum is: $SUM"
 #Just variable name has been given.
