8b31013bb4f307c3fd16078661c25eba5fb5c703
mukeshraj <mukeshraj338@gmail.com>
2026-09-03 13:16:19 +0530

    hello

--- new.sh
+++ new.sh
@@ -4,3 +4,17 @@ if [ $? -ne 0 ]; then
     echo "Please run this script with root access."   #checking to run with root acccess
     exit 1
 fi
+if [ $? -ne 0 ]; then
+    echo "Installing MySQL is... FAILED"            #checking whether mysql is already installed or not.
+    exit 1
+    else 
+    echo "Installing MySQL is... SUCCESS"
+fi
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    echo "MySQL is already installed... SKIPPING"
+    exit 1
+    else 
+    echo "Installing MySQL"
+fi
+dnf install mysql -y
