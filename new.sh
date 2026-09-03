58fdc4311ecbc26949e66e78491f80cec2be9a31
mukeshraj <mukeshraj338@gmail.com>
2026-09-03 13:38:49 +0530

    hello

--- new.sh
+++ new.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 USERID=$(id -u)
-if [ $? -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access."   #checking to run with root acccess
     exit 1
 fi
