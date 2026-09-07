69e9a08885965269852d406765ca38cb7c1cfc75
mukeshraj <mukeshraj338@gmail.com>
2026-09-07 08:44:20 +0530

    hello

--- 12-loops.sh
+++ 12-loops.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u) #condition
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOG_DIR/$0.log" 
+TIMESTAMP=$(date +%Y-%m-%d %H:%M:%S) 
 if [ $USERID -ne 0 ]; then
     echo "Please run the script with root access"
     exit 1
@@ -12,20 +13,20 @@ fi
 #second arg= exit code
 VALIDATE(){
     if [ $2 -ne 0 ]; then 
-    echo "Installing $1 is ... FAILED"
+    echo "$TIMESTAMP [ERROR] Installing $1 is ... FAILED" | tee -a $LOGS_FILE
     exit 1
 else 
-    echo "Installing $1 is ..... SUCCESSs"
+    echo "$TIMESTAMP [INFO] Installing $1 is ..... SUCCESS" | tee -a $LOGS_FILE
 fi
     }
 
     
 for package in $@
     do
-        echo "Installing $package"
+        echo "$TIMESTAMP [INFO] Installing $package"
         dnf list installed $package &>> $LOGS_FILE
         if [ $? -ne 0 ]; then
-        echo "$package already installed...SKIPPING"
+        echo "$TIMESTAMP [INFO] $package already installed...SKIPPING"
         else
         dnf install $package -y &>> $LOGS_FILE
         VALIDATE "Installing $package" $?
---
+++ 15-set.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#sudo id -u = 0
+set -e  #this is a signal to indicate error.
+USERID=$(id -u) #condition
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOG_DIR/$0.log" 
+TIMESTAMP=$(date +%Y-%m-%d %H:%M:%S)
+R="\e[30m"  #-e on every statement to enable the colors.
+G="\e[31m"
+Y="\e[32m"
+N="\e[0m" 
+trap 'echo "error at $LINENO, command: $BASH_COMMAND"' ERR #trap is a command, when the error spots ERR will get the signal & trap will execute.
+#trap is unexpected error
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with root access"
+    exit 1
+fi
+#FUCN_NAME= arg-1 arg-2
+#First arg= what are you installing.
+#second arg= exit code
+# VALIDATE(){
+#     if [ $2 -ne 0 ]; then 
+#     echo -e "$TIMESTAMP [ERROR] Installing $1 is ... $R FAILED $N" | tee -a $LOGS_FILE
+#     exit 1
+# else 
+#     echo -e "$TIMESTAMP [INFO] Installing $1 is ..... $G SUCCESS $N" | tee -a $LOGS_FILE
+# fi
+#     }
+
+    
+for package in $@
+    do
+        echo "$TIMESTAMP [INFO] Installing $package"
+        dnf list installed $package &>> $LOGS_FILE
+        if [ $? -ne 0 ]; then
+        echo -e "$TIMESTAMP [INFO] $package already installed...$Y SKIPPING $N"
+        else
+        dnf install $package -y &>> $LOGS_FILE
+        # VALIDATE "Installing $package" $?
+        fi
+    done 
