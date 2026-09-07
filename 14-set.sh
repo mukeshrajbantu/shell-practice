fb8259ab208ff2a0ae64e602eeb94af0b68c7039
mukeshraj <mukeshraj338@gmail.com>
2026-09-07 08:50:59 +0530

    hello

--- 14-set.sh
+++ 14-set.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e #this will help to stop the error, which won't continue, but won't display the info.
+# set -e #this will help to stop the error, which won't continue, but won't display the info.
 echo "HELLO WORLD >>>!!!!"
 qwertyuiop
 echo "Continuing....!??" #when the error appears the shell will continue the error.
